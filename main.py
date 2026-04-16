e7014bad78ca6b3520b3a94711b5abe407606bd9
JohnPal4 <palchizacajohn@gmail.com>
2026-04-16 13:30:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from supabase import create_client, Client
 import os
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
