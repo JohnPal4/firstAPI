7559ebf8adf3ac5c34692feb85a4b93d0f6b9685
JohnPal4 <palchizacajohn@gmail.com>
2026-04-16 20:48:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from supabase import create_client, Client
 import os
 from datetime import date
 from typing import Optional
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -26,9 +27,10 @@ class InventoryItem(BaseModel):
 
 
 # Root
-@app.get("/")
-def root():
-    return {"message": "Inventory API running 🚀"}
+@app.get("/", response_class=HTMLResponse)
+def serve_home():
+    with open("index.html", "r", encoding="utf-8") as f:
+        return f.read()
 
 
 # GET all
